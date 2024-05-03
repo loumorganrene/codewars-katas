a76889b19ad7b41dc9c3d7395788cacd504532bc
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-05-03 04:07:43 +0200

    :sparkle: solved 8kyu: finding a needle value in a haystack array + 7kyu: sorting array based on number of occurence of a same value

--- 7kyu/unscrambled-eggs.ts
+++ 7kyu/unscrambled-eggs.ts
@@ -1,3 +1,8 @@
+/**INSTRUCTIONS
+ * The string given to your function has had an "egg" inserted directly after each consonant. 
+ * You need to return the string before it became eggcoded.
+ */
+
 function unscrambleEggs(word: string): string{
   return word.replace(/egg/g, "")
 }
