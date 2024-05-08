dc60b2b94d0281f13770ec21a1038b244f872efa
Lou René <75613368+loumorganrene@users.noreply.github.com>
2024-05-08 15:47:23 +0200

    :sparkle: resolved 7kyu: Return an array of objects from an array of numbers with numbers as keys and charcode from numbers as values

---
+++ 7kyu/numbers-to-objects.js
@@ -0,0 +1,13 @@
+/**INSTRUCTIONS
+ * You will be given an array of numbers.
+ * For each number in the array you will need to create an object.
+ * The object key will be the number, as a string. 
+ * The value will be the corresponding character code, as a string.
+ * Return an array of the resulting objects.
+ * All inputs will be arrays of numbers. All character codes are valid lower case letters. 
+ * The input array will not be empty.
+ */
+
+function numObj(arr){
+  return arr.map((number)=> {return {[number]: String.fromCharCode(number)}})
+}
